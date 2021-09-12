8b96212ec234e5877e05a261078c67b8d4ccf1c7
Kamal <kk@test.com>
2021-09-13 01:33:58 +0530

    Updated to add counter example

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import FirstComponent, {SecondComponent} from './components/learning-examples/FirstComponent'
+import Counter from './components/counter/Counter'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-         My Hello World
-         <FirstComponent></FirstComponent>
-         <SecondComponent></SecondComponent>
+         <Counter></Counter>
     </div>
   );
 }
